b3d039b65653031060301115e3bdf1cdf4a479cb
CaoY <3145213279@qq.com>
2022-12-15 00:57:07 +0800

    Spring实战第一章结束

---
+++ src/main/java/com/mine/cloud/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.mine.cloud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author CaoY
+ * @date 2022-12-14 23:45
+ * @description
+ */
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        // 返回视图名
+        return "home";
+    }
+}
---
+++ src/test/java/com/mine/cloud/controller/HomeControllerTest.java
@@ -0,0 +1,35 @@
+package com.mine.cloud.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * @author CaoY
+ * @date 2022-12-14 23:55
+ * @description 测试HomeController
+ */
+// 针对HomeController的web测试
+// 该注解将HomeController 注册到 SpringMVC，使我们可以向它发请求，
+// 即我们可以不启动项目而对HomeController进行测试
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(
+                        containsString("Welcome to...")
+                ));
+    }
+}
