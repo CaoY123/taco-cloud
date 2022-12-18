861c3e50cfae19ba8fe0502736e7230ca25a96cb
CaoY123456 <caoy_6666@126.com>
2022-12-18 18:08:30 +0800

    JDBC实心数据持久化

--- src/main/java/com/mine/cloud/controller/DesignTacoController.java
+++ src/main/java/com/mine/cloud/controller/DesignTacoController.java
@@ -5,6 +5,7 @@ import com.mine.cloud.dao.TacoRepository;
 import com.mine.cloud.domain.Ingredient;
 import com.mine.cloud.domain.Order;
 import com.mine.cloud.domain.Taco;
+import com.mine.cloud.domain.TacoForm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -28,7 +29,7 @@ import java.util.stream.Collectors;
 @Slf4j
 @Controller
 @RequestMapping("/design") // 指定基本的、通用的路径
-@SessionAttributes("order")
+@SessionAttributes({"order", "ingredients"})
 public class DesignTacoController {
     // 配料表持久化
     private final IngredientRepository ingredientRepository;
@@ -64,7 +65,14 @@ public class DesignTacoController {
             model.addAttribute(type.toString().toLowerCase(),
                     filterByType(ingredients, type));
         }
+
+        model.addAttribute("ingredients", ingredients);
+
         Taco taco = new Taco();
+        List<String> ingredientNames = new ArrayList<>();
+        for (Ingredient ingredient : ingredients) {
+            ingredientNames.add(ingredient.getName());
+        }
         taco.setIngredients(ingredients);
         model.addAttribute("design", taco);
         // 跳到design.html页面
@@ -73,12 +81,32 @@ public class DesignTacoController {
 
     @PostMapping
     public String processDesign(
-            @Valid @ModelAttribute("design") Taco design,
+            @Valid TacoForm designForm,
             Errors errors,
-            @ModelAttribute Order order) {
+            @ModelAttribute Order order,
+            Model model) {
         if(errors.hasErrors()) {
             return "design";
         }
+
+        List<Ingredient> ingredients = (List<Ingredient>) model.getAttribute("ingredients");
+        List<Ingredient> tacoIngredients = new ArrayList<>();
+        // 将配料表单类（TacoForm类）进行转化为Taco类
+        List<String> ingredientNames = designForm.getIngredients();
+        for (String ingredientName : ingredientNames) {
+            for (Ingredient ingredient : ingredients) {
+                if (ingredient.getId().equals(ingredientName)) {
+                    tacoIngredients.add(ingredient);
+                }
+            }
+        }
+
+        Taco design = new Taco(
+                designForm.getId(),
+                null,
+                designForm.getName(),
+                tacoIngredients
+        );
 
         Taco saved = tacoRepository.save(design);
         order.addDesign(saved);
